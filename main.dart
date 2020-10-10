9952cba191a7d18424f4c77b0d931450f018e54d
ryosuke nakaishi <acna2676@gmail.com>
2020-10-10 20:11:23 +0900

    graph row position

--- main.dart
+++ main.dart
@@ -13,7 +13,10 @@ class MyApp extends StatelessWidget {
       home: MainPage(),
       routes: <String, WidgetBuilder>{
         '/home': (BuildContext context) => new MainPage(),
-        '/subpage': (BuildContext context) => new SubPage()
+        '/subpage': (BuildContext context) => new SubPage(),
+        '/complete': (BuildContext context) => new CompletePage(),
+        '/incomplete': (BuildContext context) => new IncompletePage(),
+        '/create': (BuildContext context) => new CreatePage(),
       },
     );
   }
@@ -24,8 +27,6 @@ class MainPage extends StatefulWidget {
   _MainPage createState() => _MainPage();
 }
 
-// class MainPage extends StatelessWidget {
-// class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   Widget chartContainer = Column(
     mainAxisAlignment: MainAxisAlignment.center,
@@ -43,17 +44,14 @@ class _MainPage extends State<MainPage> {
     });
   }
 
+  int _currentIndex = 0;
+
   GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
 
   List<BottomNavigationBarItem> myBottomNavBarItems() {
     return [
       BottomNavigationBarItem(
         icon: Icon(Icons.note_add),
-        // key: _drawerKey, // assign key to Scaffold
-        // drawer: Drawer(),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: () => _drawerKey.currentState.openDrawer(), // open drawer
-        // ),
         title: const Text('作成'),
       ),
       BottomNavigationBarItem(
@@ -92,39 +90,33 @@ class _MainPage extends State<MainPage> {
         ],
       )),
       body: new Container(
-        // height: 1000,
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              Container(
-                height: 250,
-                child: chartContainer,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Text('Bar Charts'),
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  RaisedButton(
-                    child: Text('Simple'),
-                    onPressed: () {
-                      setState(() {
-                        chartContainer = SimpleBarChart.withSampleData();
-                      });
-                    },
+                  Expanded(
+                    flex: 1, // 幅の比率
+                    child: Container(
+                      height: 150,
+                      child: SimpleBarChart.withSampleData(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 150,
+                      child: SimpleBarChart.withSampleData(),
+                    ),
                   ),
                 ],
               ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
               Text(
                 "$_text",
-                //flutter_chartのインポートが原因でTextStyleがエラーになるため一時コメントアウト
-                // style: TextStyle(
-                //     color: Colors.blueAccent,
-                //     fontSize: 30.0,
-                //     fontWeight: FontWeight.w500),
               ),
               DropdownButton<String>(
                 value: _defaultValue,
@@ -141,77 +133,21 @@ class _MainPage extends State<MainPage> {
                 onPressed: () => Navigator.of(context).pushNamed("/subpage"),
                 child: new Text('３km走る'),
               ),
-              // RaisedButton(
-              //   onPressed: () => Navigator.of(context).pushNamed("/subpage"),
-              //   child: new Text('５時間勉強する'),
-              // ),
-              // RaisedButton(
-              //   onPressed: () => Navigator.of(context).pushNamed("/subpage"),
-              //   child: new Text('１時間筋トレする'),
-              // )
             ],
           ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          // 現在のページインデックス
-          // currentIndex: _screen,
-          // // onTapでナビゲーションメニューがタップされた時の処理を定義
-          // onTap: (index) {
-          //   setState(() {
-          //     // ページインデックスを更新
-          //     _screen = index;
-
-          //     // ページ遷移を定義。
-          //     // curveで指定できるのは以下
-          //     // https://api.flutter.dev/flutter/animation/Curves-class.html
-          //     _pageController.animateToPage(index,
-          //         duration: Duration(milliseconds: 300), curve: Curves.easeOut);
-          //   });
-          // },
-          //   onTap: {
-          //     return new Scaffold(
-          // drawer: new Drawer(
-          //     child: new ListView(
-          //   children: <Widget>[
-          //     new ListTile(
-          //       title: new Text('目標一覧'),
-          //       onTap: () {},
-          //     ),
-          //     new ListTile(
-          //       title: new Text('ご褒美一覧'),
-          //       onTap: () {},
-          //     ),
-          //     new ListTile(
-          //       title: new Text('ヒント'),
-          //       onTap: () {},
-          //     ),
-          //   ],
-          // )),
-          //     )
-          //   }
-          // 定義済のナビゲーションメニューのアイテムリスト
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.note_add),
-              // key: _drawerKey, // assign key to Scaffold
-              // drawer: Drawer(),
-              // floatingActionButton: FloatingActionButton(
-              //   onPressed: () => _drawerKey.currentState.openDrawer(), // open drawer
-              // ),
-              title: const Text('作成'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: const Text('ホーム'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              title: const Text('設定'),
-            ),
-          ]),
+        // 定義済のナビゲーションメニューのアイテムリスト
+        items: myBottomNavBarItems(),
+        onTap: _onItemTapped,
+        currentIndex: _currentIndex,
+      ),
     );
   }
+
+  void _onItemTapped(int index) =>
+      setState(() => Navigator.of(context).pushNamed("/create"));
 }
 
 class SubPage extends StatelessWidget {
@@ -226,10 +162,17 @@ class SubPage extends StatelessWidget {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              Text('Sub'),
               RaisedButton(
                 onPressed: () => Navigator.of(context).pop(),
                 child: new Text('戻る'),
+              ),
+              RaisedButton(
+                onPressed: () => Navigator.of(context).pushNamed("/complete"),
+                child: new Text('完了'),
+              ),
+              RaisedButton(
+                onPressed: () => Navigator.of(context).pushNamed("/incomplete"),
+                child: new Text('あきらめる'),
               )
             ],
           ),
@@ -239,114 +182,77 @@ class SubPage extends StatelessWidget {
   }
 }
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//         // This makes the visual density adapt to the platform that you run
-//         // the app on. For desktop platforms, the controls will be smaller and
-//         // closer together (more dense) than on mobile platforms.
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+class CompletePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: new Text('戻る'),
+              ),
+              Image.asset('images/celebration.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
+class IncompletePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: new Text('戻る'),
+              ),
+              Image.asset('images/encouragement.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+class CreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              Form(
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Goal'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
